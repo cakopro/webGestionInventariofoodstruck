c833ceb1a7385542a5f279dc3a4061eea7c48c4c
Joaquin Ignacio Fernández Vasconcelo <j.fernandez130@alumnos.santotomas.cl>
2026-05-26 13:49:39 -0400

    Agregar archivos de proyecto.

---
+++ webGestionInventario/Pages/Insumos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webGestionInventario.Pages
+{
+    public class InsumosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
